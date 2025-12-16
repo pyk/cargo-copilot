1e171088ae97ea55862416c1c1998ec54cd927a1
pyk <2213646+pyk@users.noreply.github.com>
2025-12-16 14:48:27 +0700

    chore: setup basic mcp server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,67 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use rmcp::{
+    ServerHandler, ServiceExt,
+    handler::server::{tool::ToolRouter, wrapper::Parameters},
+    model::{ServerCapabilities, ServerInfo},
+    schemars, tool, tool_handler, tool_router,
+    transport::io::stdio,
+};
+
+#[derive(Debug, Default, Clone)]
+pub struct Copilot {
+    tool_router: ToolRouter<Self>,
+}
+
+#[tool_router]
+impl Copilot {
+    pub fn new() -> Self {
+        Self {
+            tool_router: Self::tool_router(),
+        }
+    }
+
+    #[tool(description = "Calculate the sum of two numbers")]
+    fn sum(&self, Parameters(SumRequest { a, b }): Parameters<SumRequest>) -> String {
+        (a + b).to_string()
+    }
+
+    #[tool(description = "Calculate the difference of two numbers")]
+    fn sub(&self, Parameters(SubRequest { a, b }): Parameters<SubRequest>) -> String {
+        (a - b).to_string()
+    }
+}
+
+#[tool_handler]
+impl ServerHandler for Copilot {
+    fn get_info(&self) -> ServerInfo {
+        ServerInfo {
+            instructions: Some("A simple calculator".into()),
+            capabilities: ServerCapabilities::builder().enable_tools().build(),
+            ..Default::default()
+        }
+    }
+}
+
+#[derive(Debug, serde::Deserialize, schemars::JsonSchema)]
+pub struct SumRequest {
+    #[schemars(description = "the left hand side number")]
+    pub a: i32,
+    pub b: i32,
+}
+
+#[derive(Debug, serde::Deserialize, schemars::JsonSchema)]
+pub struct SubRequest {
+    #[schemars(description = "the left hand side number")]
+    pub a: i32,
+    #[schemars(description = "the right hand side number")]
+    pub b: i32,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    println!("Starting cargo-copilot");
+    let service = Copilot::new().serve(stdio()).await?;
+
+    service.waiting().await?;
+    Ok(())
 }
